e9b22ef73d080fe347b694e9c6131743a8b0b213
Alejandro Kondrasky <drasky.vanderhoff@gmail.com>
2019-02-13 21:58:31 -0300

    Development process complete in a 80%, added react components and redux reducer for saving the selected color text. Missing is showing in editable text area the selected text and also being able to select multiple color filter. I'll add that last one soon. Unit test will come in next commit.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { connect } from 'react-redux'
+import { saveSelectedText } from 'actions'
+import InteractiveColorRow from 'components/InteractiveColorRow';
+import InputBox from 'components/InputBox';
+import ColorTextPresenter from 'components/ColorTextPresenter';
+import config from './config.json';
+import './App.scss';
 
-class App extends Component {
+class App extends Component {  
+  constructor(props) {
+    super(props);
+    const highlightedColor = config.availableColorPairs.find(
+      ({isHighlighted}) => isHighlighted
+    ).background || "";
+    this.state = {
+      selectionColor: highlightedColor,
+      filterColor: highlightedColor
+    };
+  }
+
+  saveSelectedText = (selectionStart, selectionEnd) => {this.props.saveSelectedText(
+    this.state.selectionColor,
+    selectionStart,
+    selectionEnd,
+    config.text
+  )}
+
+  setSelectionColor = (selectionColor) => this.setState({selectionColor});
+  setFilterColor = (filterColor) => this.setState({filterColor});
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+        <InteractiveColorRow 
+          colorBoxesProps={config.availableColorPairs} 
+          onSelectedColorRow={this.setSelectionColor}
+          descriptionText={config.selectorDescriptionText}
+          />
+        <InputBox text={config.text} onSelectedText={this.saveSelectedText} />
+        <InteractiveColorRow 
+          colorBoxesProps={config.availableColorPairs} 
+          onSelectedColorRow={this.setFilterColor} 
+          descriptionText={config.filterDescriptionText}
+        />
+        <ColorTextPresenter colorTextItems={{[this.state.filterColor]: this.props.higlightedTextItems[this.state.filterColor]}} />
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  higlightedTextItems: state.higlightedTextItems
+})
+
+const mapDispatchToProps = dispatch => ({
+  saveSelectedText: (
+    ...params
+  ) => dispatch(saveSelectedText(
+    ...params
+  ))
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App)
